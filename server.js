2daaeafd86a51c4d7559b9cebb5275c96f34464f
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-10-22 20:31:23 -0400

    add mongoose to server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network", {
+    useFindAndModify: false,
+})
