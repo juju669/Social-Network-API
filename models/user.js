cea36c6d33abdb1793d40e1d6ed605010552b4f7
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-10-23 08:08:35 -0400

    user scehma created

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const { isEmail } =  require('validator')
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      minLength: 6,
+      maxLength: 16,
+      required: [true, "this need to be fill"],
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "please add a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+
+const User = model("User", UserSchema);
+module.exports = User;
