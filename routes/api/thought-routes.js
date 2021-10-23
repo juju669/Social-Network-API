bdcd10d80d22796b2e696e47ee245855b9d89a0f
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-10-23 08:33:40 -0400

    api routes complete

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+
+const{
+    getAllThought,
+    getThoughtByID,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require("../../controllers/thought-controller");
+
+router.route("/")
+.get(getAllThught);
+
+router.route("/")
+    .get(getAllThought);
+
+router.route('/:userId')
+    .post(createThought);
+
+router.route("/:thoughtId")
+    .get(getThoughtById)
+    .put(updateThought);
+
+router.route("/:userId/:thoughtId")
+    .delete(deleteThought);
+
+router.route("/:thoughtId/reactions")
+    .post(createReaction)
+    
+
+router.route("/:thoughtId/reactions/:reactionId")
+    .delete(deleteReaction)
+
+module.exports = router
