4545170181f117559d5b8fa5b7600bd077ddc331
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-10-23 08:01:46 -0400

    thought schema created

---
+++ models/thought.js
@@ -0,0 +1,72 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+// <== ! Thought Schema Below !==>
+
+const ThoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [2, " 2 or more characters required"],
+        maxLength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+      username: {
+        type: String,
+        required: [true, "this need to be fill"],
+        trim: true,
+        minLength: 6,
+        maxLength: 16,
+      },
+      reactions: [ReactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  
+  ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model("Thought", ThoughtSchema);
+  module.exports = Thought;
